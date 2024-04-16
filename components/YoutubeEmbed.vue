5431ac56c2e09dcf7df637212f5391323fa9f51f
hecticme <hangoc.minh1306@gmail.com>
2024-04-16 15:21:03 +0700

    feat: create an embedded player

--- components/YoutubeEmbed.vue
+++ components/YoutubeEmbed.vue
@@ -6,8 +6,45 @@ useHead({
     },
   ],
 })
+
+const videoTitle = ref('Vapor Trail')
+
+onMounted(() => {
+  function onYouTubeIframeAPIReady () {
+    // TODO: Declare type for "YT" object.
+    // @ts-expect-error
+    const player = new YT.Player('youtube-player', {
+      type: 'text/html',
+      events: {
+        onReady: () => {
+          console.log('Ready!')
+        },
+        onStateChange: () => {
+          console.log('Something changed.')
+        },
+      },
+    })
+
+    return player
+  }
+
+  onYouTubeIframeAPIReady()
+})
 </script>
 
 <template>
-  Youtube
+  <section class="youtube-wrapper">
+    <h1 class="youtube-title">
+      {{ videoTitle }}
+    </h1>
+
+    <iframe
+      id="youtube-player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+      referrerpolicy="strict-origin-when-cross-origin"
+      src="https://www.youtube.com/embed/24g3l_IJuIE?enablejsapi=1"
+      :title="videoTitle"
+    />
+  </section>
 </template>
