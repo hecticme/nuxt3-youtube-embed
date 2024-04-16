8131ee08ebaebfa31568e09edfd461ce34e672b8
hecticme <hangoc.minh1306@gmail.com>
2024-04-16 13:38:25 +0700

    feat: create YoutubeEmbed component

---
+++ components/YoutubeEmbed.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: 'Youtube Embedded Iframe',
+  script: [
+    {
+      src: 'https://www.youtube.com/iframe_api',
+    },
+  ],
+})
+</script>
+
+<template>
+  Youtube
+</template>
